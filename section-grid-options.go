d447c03fa4d4052c270d86d84711d0d39bc33c5c
Ozan Kasikci <kasikciozan@yandex.com>
2019-11-28 21:06:14 +0100

    Add section-grid-options.go

---
+++ section-grid-options.go
@@ -0,0 +1,53 @@
+package gimgui
+
+import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+	"strconv"
+)
+
+func (t *Gim) GridSize() *widget.Box {
+	onSizeChange := func(enum rune) func(string) {
+		return func(s string) {
+			i, err := strconv.Atoi(s)
+			if s != "" && err != nil {
+				fyneDialog.ShowError(errors.New("Please enter a digit"), *t.Window)
+			}
+
+			if enum == 'x' {
+				if s == "" {
+					i = DefaultGridCountX
+				}
+				t.GridCountX = i
+			} else {
+				if s == "" {
+					i = DefaultGridCountY
+				}
+				t.GridCountY = i
+			}
+
+			t.generateGrids()
+			t.generateCanvasObjectsFromGrids()
+		}
+	}
+
+	sizeEntryX := widget.NewEntry()
+	sizeEntryX.OnChanged = onSizeChange('x')
+	sizeEntryX.SetPlaceHolder(strconv.Itoa(DefaultGridCountX))
+
+	sizeEntryY := widget.NewEntry()
+	sizeEntryY.OnChanged = onSizeChange('y')
+	sizeEntryY.SetPlaceHolder(strconv.Itoa(DefaultGridCountY))
+
+	return widget.NewVBox(widget.NewHBox(
+		widget.NewLabel("Horizontal Size:"),
+		sizeEntryX,
+		widget.NewLabel("Vertical Size:"),
+		sizeEntryY,
+	))
+}
+
+func (t *Gim) GridOptionsSection() *fyne.Container {
+	return fyne.NewContainerWithLayout(layout.NewGridLayout(1), widget.NewGroup("Grid Options", t.GridSize()))
+}
