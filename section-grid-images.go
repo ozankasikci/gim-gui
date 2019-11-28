6304724ee0c9454f8bcd71f53b13a5acdc3ea1a7
Ozan Kasikci <kasikciozan@yandex.com>
2019-11-28 21:06:14 +0100

    Add section-grid-images.go

---
+++ section-grid-images.go
@@ -0,0 +1,26 @@
+package gimgui
+
+import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+)
+
+func (t *Gim) GridImagesSection() *fyne.Container {
+
+	images := fyne.NewContainerWithLayout(
+		layout.NewVBoxLayout(),
+	)
+
+	t.ImagesSection = images
+	t.generateGrids()
+	t.generateCanvasObjectsFromGrids()
+
+	return fyne.NewContainerWithLayout(
+		layout.NewGridLayout(1),
+		widget.NewGroup("Grids",
+			images,
+		),
+	)
+}
+
