513b0a71fd2b8c3081ead9327aeac9a649c6af3e
Ozan Kasikci <kasikciozan@yandex.com>
2019-12-23 23:40:53 +0300

    update constants

--- constants.go
+++ constants.go
@@ -7,13 +7,19 @@ const (
 	MaxGridCountY       = 9
 	DefaultGridSizeX    = 75
 	DefaultGridSizeY    = 75
+	DefaultOutputSizeX  = 400
+	DefaultOutputSizeY  = 400
 	PreviewImageMaxSize = 400.0
 
+	ErrorPreview = "Error, please make sure that you've filled all the slots"
+
 	TextEnterDigitWarning = "Please enter a digit"
-	TextColumnCount       = "Column Item Count:"
-	TextRowCount          = "Row Item Count:"
+	TextColumnCount       = "Column Count:"
+	TextRowCount          = "Row Count:"
 	TextGridSizeX         = "Image Size X (px):"
 	TextGridSizeY         = "Image Size Y (px):"
+	TextOutputSizeX       = "Output Size X (px):"
+	TextOutputSizeY       = "Output Size Y (px):"
 
 	TitleTabItemGridCount = "Grid Item Count"
 	TitleTabItemGridSize  = "Grid Image Size"
